189887e0d61ff28d5e75a2abbacbeda2e363a43a
Vytautas Saulis <vaulis11@gmail.com>
2022-03-07 02:03:11 +0200

    Changes

--- ios/heritage_4_life/AppDelegate.m
+++ ios/heritage_4_life/AppDelegate.m
@@ -30,8 +30,10 @@ static void InitializeFlipper(UIApplication *application) {
 {
   [GMSServices provideAPIKey:@"AIzaSyCEkBZYaO2jMCuPF00QsIkmpyNbKcILS-k"];
 
-  #ifdef FB_SONARKIT_ENABLED
-    InitializeFlipper(application);
+  #if DEBUG
+    #ifdef FB_SONARKIT_ENABLED
+      InitializeFlipper(application);
+    #endif
   #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
