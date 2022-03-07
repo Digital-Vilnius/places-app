5b6caaffd8af2bc21269cb5025c7bc5c2b21fd11
Vytautas Saulis <vaulis11@gmail.com>
2022-03-07 11:45:09 +0200

    Push notifications

--- src/features/places/screens/PlacesScreen.tsx
+++ src/features/places/screens/PlacesScreen.tsx
@@ -1,13 +1,8 @@
 import React, { FC } from 'react';
-import { ScreenContainer } from '@components';
 import { Places } from '../hoc';
 
 const PlacesScreen: FC = () => {
-  return (
-    <ScreenContainer>
-      <Places />
-    </ScreenContainer>
-  );
+  return <Places />;
 };
 
 export default PlacesScreen;
