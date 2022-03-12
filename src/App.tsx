d53f5c0f086e9d0ea22fd7c47c26b73890e29b06
Vytautas Saulis <vaulis11@gmail.com>
2022-03-13 00:40:07 +0200

    Splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { QueryClientProvider } from 'react-query';
 import queryClient from '@core/query';
@@ -10,11 +10,16 @@ import { setupTranslations } from '@core/translations';
 import { setupNotifications } from '@core/notifications';
 import { Platform } from 'react-native';
 import { CurrentLocationProvider } from '@core/location/provider';
+import SplashScreen from 'react-native-splash-screen';
 
 setupNotifications();
 setupTranslations();
 
 const App: FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
